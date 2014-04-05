73e2a920f25998096549da3901899f2e1f94d3c9
Ryan Bigg <radarlistener@gmail.com>
2014-04-05 12:39:21 +1100

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.create!(
+  :title => "Getting Started with Ember", 
+  :text => "TODO: Getting started with Ember guide"
+)
+
+Post.create!(
+  :title => "Light Angular trolling",
+  :text => "TODO: think of pithy things to say about Angular")
