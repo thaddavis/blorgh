122dea2d648c006a9f19f55f69acecdf5ea79a69
Ryan Bigg <radarlistener@gmail.com>
2014-04-06 07:28:03 +1000

    Add create action to posts controller

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -6,4 +6,19 @@ class Api::PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      render :show, :status => 201
+    else
+      render "api/shared/errors", resource: @post, status: 422
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
